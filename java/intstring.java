80228e57a9cde41ad0e018c3f1dae1ed327165cc
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-02 14:31:04 +0530

    Implement myAtoi method for string to integer conversion

---
+++ java/intstring.java
@@ -0,0 +1,28 @@
+class intstring {
+    public int myAtoi(String s) {
+        int index = 0;
+        int sign = 1;
+        int result = 0;
+        int n = s.length();
+        while (index < n && s.charAt(index) == ' ') {
+            index++;
+        }
+    
+        if (index < n && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
+            sign = s.charAt(index) == '+' ? 1 : -1;
+            index++;
+        }
+    
+        while (index < n && Character.isDigit(s.charAt(index))) {
+            int digit = s.charAt(index) - '0';
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            
+            result = result * 10 + digit;
+            index++;
+        }
+        
+        return result * sign;
+    }
+}
