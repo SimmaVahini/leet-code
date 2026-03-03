3328efa2194256e89a56cd6d93d343d025675a23
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-03 09:50:44 +0530

    Add letterCombinations method to Solution class
    Implement letter combinations for phone digits.

---
+++ python/letter combination.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+        
+       
+        phone = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        
+        result = []
+        
+        def backtrack(index, path):
+           
+            if len(path) == len(digits):
+                result.append("".join(path))
+                return
+            
+          
+            possible_letters = phone[digits[index]]
+            
+            for letter in possible_letters:
+    
+                path.append(letter)
+                backtrack(index + 1, path)
+                
+                path.pop()
+        
+        backtrack(0, [])
+        return result
