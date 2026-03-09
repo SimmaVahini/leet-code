2231eeacbcd381d516cbf194bcf92a9977a731bd
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-09 09:26:37 +0530

    Create sqrt.py

---
+++ python/sqrt.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mySqrt(self, x):
+        low = 0
+        high = x
+        ans = 0
+        
+        while low <= high:
+            mid = (low + high) 
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+                
+        return ans
