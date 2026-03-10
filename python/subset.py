8744b5dd13afb3cceb96a3f13574bec9e9e12619
SimmaVahini <rsimmavahini2005@gmail.com>
2026-03-10 09:30:39 +0530

    Create subset.py

---
+++ python/subset.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def subsets(self, nums):
+        result = [[]]
+        for num in nums:
+           
+            result += [curr + [num] for curr in result]
+        return result
