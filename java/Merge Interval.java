f0867d78c5828c8ece27f4144098843249145664
SimmaVahini <rsimmavahini2005@gmail.com>
2026-08-04 19:22:54 +0530

    Implement merge intervals algorithm in Java

---
+++ java/Merge Interval.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        // Step 1: Sort intervals based on start time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        // Step 2: Start with first interval
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        // Step 3: Check remaining intervals
+        for (int i = 1; i < intervals.length; i++) {
+
+            // Overlapping
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            }
+
+            // Not overlapping
+            else {
+                result.add(new int[]{start, end});
+
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        // Add last interval
+        result.add(new int[]{start, end});
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
