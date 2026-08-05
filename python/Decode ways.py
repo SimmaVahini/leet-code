689cc0aeae78a402d7a81ad39a203a33acd55e82
SimmaVahini <rsimmavahini2005@gmail.com>
2026-08-05 11:31:01 +0530

    Implement decoding ways algorithm in Decode ways.py

---
+++ python/Decode ways.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numDecodings(self, s):
+        if not s or s == '0':
+            return 0
+            
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        for i in range(2, n + 1):
+            # Check single digit
+            one_digit = int(s[i-1:i])
+            if 1 <= one_digit <= 9:
+                dp[i] += dp[i-1]
+                
+            # Check two digits
+            two_digits = int(s[i-2:i])
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i-2]
+                
+        return dp[n]
