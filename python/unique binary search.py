5ec0044a3c8b1bf979dc9431d2a33f93b2e24143
SimmaVahini <rsimmavahini2005@gmail.com>
2026-08-05 12:08:25 +0530

    Refactor unique binary search tree function

--- python/unique binary search.py
+++ python/unique binary search.py
@@ -1,47 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution(object):
-    def generateTrees(self, n):
+    def numTrees(self, n):
         """
         :type n: int
-        :rtype: List[TreeNode]
+        :rtype: int
         """
-        if n == 0:
-            return []
+        # dp[i] stores the number of unique BSTs that can be formed with i nodes
+        dp = [0] * (n + 1)
+        dp[0] = 1  # Empty tree
+        dp[1] = 1  # Single node tree
         
-        memo = {}
-        
-        def buildTrees(start, end):
-            # Base case: if start > end, there are no elements to form a tree.
-            # Return a list containing None (representing an empty subtree).
-            if start > end:
-                return [None]
+        # Fill the DP table
+        for nodes in range(2, n + 1):
+            for root in range(1, nodes + 1):
+                left_subtrees = dp[root - 1]
+                right_subtrees = dp[nodes - root]
+                dp[nodes] += left_subtrees * right_subtrees
                 
-            if (start, end) in memo:
-                return memo[(start, end)]
-                
-            all_trees = []
-            
-            # Pick a root node i
-            for i in range(start, end + 1):
-                # Generate all left and right subtrees
-                left_trees = buildTrees(start, i - 1)
-                right_trees = buildTrees(i + 1, end)
-                
-                # Connect left and right subtrees to the root i
-                for left in left_trees:
-                    for right in right_trees:
-                        root = TreeNode(i)
-                        root.left = left
-                        root.right = right
-                        all_trees.append(root)
-                        
-            memo[(start, end)] = all_trees
-            return all_trees
-            
-        return buildTrees(1, n)
+        return dp[n]
